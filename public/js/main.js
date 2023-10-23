0311986f23958165a4d3604283a8de27ba41126b
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-10-22 23:10:57 -0700

    Updates styles

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,15 @@
-const emailInput = document.getElementById("email");
-const emailConfirmationInput = document.getElementById("email-confirm");
-const contactForm = document.getElementById("contact-form");
+const emailInput = document.getElementById('email');
+const emailConfirmationInput = document.getElementById('email-confirm');
+const contactForm = document.getElementById('contact-form');
 
 function validateEmailMatch() {
   const { value: emailText } = emailInput;
   const { value: confirmationText } = emailConfirmationInput;
 
   if (emailText && confirmationText && emailText !== confirmationText) {
-    errorMsg = "Sorry, your email addresses don't match. Please try again.";
+    errorMsg = 'Sorry, your email addresses don\'t match. Please try again.';
     alert(errorMsg);
-    emailConfirmationInput.value = "";
+    emailConfirmationInput.value = '';
   }
 }
+
